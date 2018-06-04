5b764b5473821fcc598a417493649b8f585b9a7c
lzy <lazylee7777@163.com>
2018-06-04 23:48:46 +0800

    change

--- app/src/main/java/com/lazylee/lzywanandroid/activity/web/WebActivity.java
+++ app/src/main/java/com/lazylee/lzywanandroid/activity/web/WebActivity.java
@@ -4,6 +4,8 @@ package com.lazylee.lzywanandroid.activity.web;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import com.lazylee.lzywanandroid.R;
@@ -16,8 +18,13 @@ public class WebActivity extends AppCompatActivity {
 
     private WebView mWebView;
 
+    private boolean jsEnable = true;
+
     private void initViews(){
         mWebView = findViewById(R.id.webView);
+        mWebView.setWebChromeClient(new WebChromeClient());
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptEnabled(jsEnable);
 
     }
     @Override
